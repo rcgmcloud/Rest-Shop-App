d008d733df7f33cc1eeea6aa2534e6e51aca8b42
Raymond Mariano <rcm@hawaii.edu>
2015-08-01 21:51:47 -1000

    make form work

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,21 @@
-console.log("Anything");
+$(function(){
+  $.get( "http://localhost:8080/products", function( data ) {
+  console.log(data);
+  });
+
+  $("#productForm").submit(function(event){
+    event.preventDefault();
+    var dataArray = $(this).serializeArray();
+
+    var data = {};
+
+    dataArray.forEach(function(x){
+      data[x.name] = x.value;
+    });
+
+    console.log('form was submitted', data);
+    $.post(this.action, JSON.stringify(data), function(data, textStatus, jqXHR){
+      console.log('everything is good');
+    });
+  });
+});
--- server.js
+++ server.js
@@ -4,15 +4,35 @@ var app = express();
 var pg = require('pg');
 var hstore = require('pg-hstore')();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 app.set ('views', './views');
 app.set ('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res, next) {
-  res.render('index');
+
+var restify = require('restify');
+
+var client = restify.createJsonClient({
+  url: 'http://localhost:8080',
+  version: '~1.0'
 });
+
+
+
+app.get('/', function (req, res, next) {
+  client.get('http://localhost:8080/products', function (err, _req, _res, obj) {
+    return res.render('index', {products: obj});
+  });
+});
+
+app.get('/products/:id', function (req, res, next) {
+  client.get('http://localhost:8080/products/' + req.params.id, function (err, _req, _res, obj) {
+    return res.render('product_view', {products: obj});
+  });
+});
+
 
 app.listen(3000, function () {
  console.log("running");
